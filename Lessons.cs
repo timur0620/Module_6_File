f64f86e472c6f31ba1a2fd25acbf0d94b14879be
Timur <timur0620@list.ru>
2024-02-02 15:02:25 +0300

    Добавьте файлы проекта.

---
+++ Lessons.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Module_6_File
+{
+    internal class Lessons
+    {
+        static void MathDatetime()
+        {
+            DateTime date = new DateTime(2020, 09, 28, 01, 30, 00);
+
+            date.ToShortTimeString();
+            date.ToShortDateString();
+
+            date = DateTime.Now;
+            static void SomeFunc()
+            {
+                for (int i = 0; i < 1_000_000; i++)
+                {
+                    double z = Math.PI * i;
+                }
+            }
+            TimeSpan timeSpan = DateTime.Now.Subtract(date);
+
+            double workTime = timeSpan.TotalMicroseconds;
+        }
+        static void StringFunc()
+        {
+            string str = "JKNKNLKNLMNLmm,n";
+            str.IndexOf('J');
+            
+
+            str.Insert(str.IndexOf(' ') + 1, "lklk");
+            str.Remove(10);
+
+            String.IsNullOrEmpty(str);
+
+            char c = 'a';
+            Char.IsDigit(c);
+            Char.IsLetter(c);
+            Char.IsLetterOrDigit(c);
+        }
+        static void FileFunc()
+        {
+            string text = File.ReadAllText("F:\\c#Projects\\Module_6_File\\TestText\\Text.txt");
+            //Console.WriteLine(text);
+
+            string[] textLine = File.ReadAllLines("F:\\c#Projects\\Module_6_File\\TestText\\Text.txt");
+            //Console.WriteLine(String.Concat(textLine));
+
+            File.WriteAllText("F:\\c#Projects\\Module_6_File\\TestText\\Text_2.txt", text);
+            File.WriteAllLines("F:\\c#Projects\\Module_6_File\\TestText\\Text_3.txt", textLine);
+
+            File.AppendAllLines("F:\\c#Projects\\Module_6_File\\TestText\\Text.txt", textLine);
+            File.AppendAllText("F:\\c#Projects\\Module_6_File\\TestText\\Text.txt", text);
+
+            //File.Copy("F:\\c#Projects\\Module_6_File\\TestText\\Text_3.txt", "F:\\c#Projects\\Module_6_File\\TestText\\Text.txt");
+            //File.Delete("");
+            //File.Exists("");
+            //File.Move("", "");
+        }
+        static void __SrteamWriter()
+        {
+            var dirs  = new DirectoryInfo(@"c:\").GetDirectories();
+
+
+            StreamWriter streamWriter = new StreamWriter("F:\\c#Projects\\Module_6_File\\TestText\\newTextFile.txt");
+            foreach (var item in dirs)
+            {
+                 streamWriter.WriteLine(item.Name);
+            }
+            streamWriter.Close();
+
+            using (StreamWriter sw = new StreamWriter("newTextFile.txt"))
+            {
+                foreach (var item in dirs)
+                {
+                    sw.WriteLine(item.Name);
+                }
+            }
+            using (StreamReader sr = new StreamReader("newTextFile.txt"))
+            {
+                 while(!sr.EndOfStream)
+                {
+                    Console.WriteLine(sr.Read());
+                }
+            }
+        }
+        static void StringBuilder()
+        {
+            StringBuilder sb = new StringBuilder("kjlkjhjkhkjhkjh");
+            sb.Append("k");
+            sb.AppendLine("lklklk ;l;");
+            sb.Remove(1, 4);
+            sb.Replace("d", "a");
+            sb[1] = 's';
+            
+        }
+    }   
+}
---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Bogus;
+using System.IO;
+
+
+namespace Module_6_File
+{
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+            string path = @"F:\c#Projects\Module_6_File\FileFromHomeWork\PersonData.txt";
+
+            DataController(path);
+        }
+        static string[,] AddStatusEmployer()
+        {
+            Console.WriteLine("Enter the employee's data");
+                            
+            var faker = new Faker("sk");
+            Console.Write(faker.Name.FullName());
+
+            string[,] dataEmployer = new string[,] { { "id", "null"},
+                                                     { "date registration", "null"},
+                                                     { "full name","null" },
+                                                     { "height", "null" },
+                                                     { "date of birth", "null" },
+                                                     { "place of birth", "null" } 
+                                                     };
+
+            for (int i = 0; i < dataEmployer.Length / 2; i++)
+            {   
+                Console.Write($" {dataEmployer[i, 0]}_____ ".ToUpper());
+                string someData = Console.ReadLine();
+
+                dataEmployer[i, 1] = someData;
+
+                if (someData.Equals(""))
+                {   
+                    Console.WriteLine($"Random employee data {dataEmployer[i, 0]}".ToUpper());
+                    Random random = new Random();
+
+                    if (dataEmployer[i, 0] == "id") dataEmployer[i, 1] = int.MaxValue.ToString();
+                    else if (dataEmployer[i, 0] == "date registration") dataEmployer[i, 1] = DateTime.Now.ToString();
+                    else if (dataEmployer[i, 0] == "full name") dataEmployer[i, 1] = faker.Person.FullName;
+                    else if (dataEmployer[i, 0] == "height") dataEmployer[i, 1] = random.Next(150, 180).ToString();
+                    else if (dataEmployer[i, 0] == "date of birth") dataEmployer[i, 1] = faker.Person.DateOfBirth.ToString();
+                    else if (dataEmployer[i, 0] == "place of birth") dataEmployer[i, 1] = faker.Person.Address.City;
+                }
+            }
+            return dataEmployer;
+        }
+        static void RecordInFile(string[,] dataPerson, string path)
+        {          
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.Write("<> ");
+                    for (int i = 0; i < dataPerson.Length / 2; i++)
+                    {
+                        sw.Write(dataPerson[i, 0].ToUpper() + "-->" + dataPerson[i, 1] + " | ");
+                    }
+                    sw.Write(" <>");
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.Write('\n');
+                    sw.Write("<> ");
+                    for (int i = 0; i < dataPerson.Length / 2; i++)
+                    {
+                        sw.Write(dataPerson[i, 0].ToUpper() + "-->" + dataPerson[i, 1] + " | ");
+                    }
+                    sw.Write(" <>");
+                }
+            }
+            
+        }
+        static void DataController (string path)
+        {
+            Console.Write(" Press 1 if you want to view the data \n " +
+                           "Press 2 if you want to entered data \n");
+
+            char choice = char.Parse (Console.ReadLine());
+
+            if (choice == '1')
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string s = "";
+
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+            }
+            else if (choice == '2')
+            {
+                string[,] dataEmployer = AddStatusEmployer();
+
+                RecordInFile(dataEmployer, path);
+            }
+            else
+            {
+                Console.WriteLine("Error!!! Try again");
+            }
+        }
+    }
+}
